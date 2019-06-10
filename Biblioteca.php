4b38f213197d2a3df2efa863c13011134b51444c
fily666 <wilmarfeliper@gmail.com>
2019-06-10 15:46:28 -0500

    first commit

---
+++ Biblioteca.php
@@ -0,0 +1,25 @@
+ <?php
+ if(isset($_POST['enviar'])){
+ function operaciones($num1, $num2, $oper){ 
+     
+$num1=$_POST['primero'];
+$num2=$_POST['segundo'];
+$oper=$_POST['operador'];
+
+  if($oper=='s'){
+   $resul=$num1+$num2;
+   echo "El resultado de la suma es: " . $resul;
+  }elseif($oper=='r'){
+   $resul=$num1-$num2;
+   echo "El resultado de la resta es: ".$resul;
+  } elseif($oper=='m'){
+   $resul=$num1*$num2;
+   echo "El resultado de la multiplicacion es: ".$resul;
+  } elseif($oper=='d'){
+   $resul=$num1/$num2;
+   echo "El resultado de la divison es: ".$resul;
+  }
+ }
+ operaciones($_POST['primero'], $_POST['segundo'],$_POST['operador'] );
+}
+?>
---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Operaciones basicas</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+    <div class="container">
+        
+        <div class="panel panel-primary">
+            <div class="panel-heading"> Curso de php </div>
+            <div class="panel-body">
+                <div class="container">
+              
+<?php
+echo"nombre<br>";
+echo"nombre<br>";
+echo"nombre<br>";
+?>
+                </div>
+                <div class="col-lg-8">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">Operaciones Basicas </div>
+                             <div class="panel-body">
+                        
+                            
+                            <form id="form1" name="form1" method="post" action="">
+                                
+                                ingresar numero 1 <br>
+                                <input class="form-control" type="number" name="primero" required /> <br>
+                                Operaciones <br>
+                                <select class="form-control" name="operador" required>
+                                    <option value="">-Seleccion-</option>
+                                    <option value="s">S</option>
+                                    <option value="r">R</option>
+                                    <option value="m">M</option>
+                                    <option value="d">D</option>
+                                </select><br>
+                                ingresar numero 2 <br>
+                                <input class="form-control" type="number" name="segundo" required /><br>
+                                <input type="submit" name="enviar" value="Resultado" class="btn-success" />
+                                
+                            </form>
+                            
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-lg-4">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            
+                            Nota: Operaciones Básicas simbología <br><br>
+                            
+                            Suma (+) = S <br>
+Resta (-) = R <br>
+Multiplicación (*) = M<br>
+División (/) = D
+
+                        </div>
+                        <div class="panel-body">
+<div class="alert alert-success">
+  
+ <?php
+ 
+  require_once 'Biblioteca.php';
+    
+?>
+                
+  
+</div>               
+
+                              </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+</body>
+
+</html>
