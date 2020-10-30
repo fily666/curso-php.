85849272b88618eb0f3b8041f10d93190dd1a304
wilmar.rincon <wilmar.rincon@linktic.com>
2020-10-30 11:00:52 -0500

    modificacion formato de codigo

--- G3/index.php
+++ G3/index.php
@@ -10,30 +10,31 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
+
 <body>
 
     <div class="container">
-        
+
         <div class="panel panel-primary">
             <div class="panel-heading"> Curso de php </div>
             <div class="panel-body">
                 <div class="container">
-   
+
                     Wilmar Felipe <br>
                     Rincon castañeda <br>
                     Desarrollo web con PHP<br>
                     Evidencia: Taller "Uso de funciones"<br>
 
-                   
+
                 </div>
                 <div class="col-lg-8">
                     <div class="panel panel-info">
                         <div class="panel-heading">Operaciones Basicas </div>
-                             <div class="panel-body">
-                        
-                            
+                        <div class="panel-body">
+
+
                             <form id="form1" name="form1" method="post" action="">
-                                
+
                                 ingresar numero 1 <br>
                                 <input class="form-control" type="number" name="primero" required /> <br>
                                 Operaciones <br>
@@ -47,42 +48,42 @@
                                 ingresar numero 2 <br>
                                 <input class="form-control" type="number" name="segundo" required /><br>
                                 <input type="submit" name="enviar" value="Resultado" class="btn-success" />
-                                
+
                             </form>
-                            
+
                         </div>
                     </div>
                 </div>
-                
+
                 <div class="col-lg-4">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            
+
                             Nota: Operaciones Básicas simbología <br><br>
-                            
+
                             Suma (+) = S <br>
-Resta (-) = R <br>
-Multiplicación (*) = M<br>
-División (/) = D
+                            Resta (-) = R <br>
+                            Multiplicación (*) = M<br>
+                            División (/) = D
 
                         </div>
                         <div class="panel-body">
-<div class="alert alert-success">
-  
- <?php
- 
-  require_once 'Biblioteca.php';
-    
-?>
-                
-  
-</div>               
+                            <div class="alert alert-success">
 
-                              </div>
+                                <?php
+
+                                require_once 'Biblioteca.php';
+
+                                ?>
+
+
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
-            
+
         </div>
     </div>
 
